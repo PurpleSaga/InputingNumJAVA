1618c96981cc1ba4bba60455102bf04c158077e8
Snehal <94309701+PurpleSaga@users.noreply.github.com>
2021-12-14 06:20:06 -0800

    Add files via upload
    in this java program l take run time input for integers
    
    starting with declaring the class and specifying it's name
    define the main method declaring the the integer variables
    a storing the runtime input syntax i.e. Integer.parseInt  
    same with storing the b runtime input syntax 
    then add the addition of a & b in c and print it
    that's it

---
+++ InputNumJAVA.java
@@ -0,0 +1,12 @@
+public class InputNumJAVA 
+{
+ public static void main(String args[])
+ {
+	 int a,b,c;
+	 a=Integer.parseInt(args[0]);
+	 b=Integer.parseInt(args[1]);
+	 c=a+b;
+	 System.out.println("the addition is "+c);
+	 
+ }
+}
